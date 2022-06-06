3f0ae2a5f548e0d5fa99fa26f7fe79b51a5904c2
Abhishek Yadav <36642139+AbhishekYadavZ@users.noreply.github.com>
2022-06-06 12:59:10 +0000

    main

---
+++ login_system/urls.py
@@ -0,0 +1,12 @@
+
+
+from django.contrib import admin
+from django.urls import path, include
+from App import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.frontend, name="frontend"),
+    path('backend/', views.backend, name="backend"),
+    path('login/', include('django.contrib.auth.urls')),
+]
